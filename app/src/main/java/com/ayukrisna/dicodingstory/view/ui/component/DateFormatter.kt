97017d4b499ae92aac65b2a06dc89ecfd86ae979
ayuukrisn <ayuukrisn@gmail.com>
2024-11-27 15:10:35 +0800

    added time to detail story

---
+++ app/src/main/java/com/ayukrisna/dicodingstory/view/ui/component/DateFormatter.kt
@@ -0,0 +1,14 @@
+package com.ayukrisna.dicodingstory.view.ui.component
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun formatTimestamp(timestamp: String): String {
+    val parsedDate = ZonedDateTime.parse(timestamp)
+    val formatter = DateTimeFormatter.ofPattern("MMMM d, yyyy, hh:mm a", Locale.getDefault())
+    return parsedDate.format(formatter)
+}
--- app/src/main/java/com/ayukrisna/dicodingstory/view/ui/screen/detailstory/DetailStoryScreen.kt
+++ app/src/main/java/com/ayukrisna/dicodingstory/view/ui/screen/detailstory/DetailStoryScreen.kt
@@ -1,13 +1,19 @@
 package com.ayukrisna.dicodingstory.view.ui.screen.detailstory
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,19 +21,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import com.ayukrisna.dicodingstory.R
 import com.ayukrisna.dicodingstory.data.remote.response.Story
 import com.ayukrisna.dicodingstory.util.Result
 import com.ayukrisna.dicodingstory.view.ui.component.CenterAppBar
 import com.ayukrisna.dicodingstory.view.ui.component.LoadingProgress
+import com.ayukrisna.dicodingstory.view.ui.component.formatTimestamp
 import org.koin.androidx.compose.koinViewModel
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun DetailStoryScreen (
     id: String,
@@ -62,7 +73,8 @@ fun DetailStoryScreen (
                         DetailStory(
                             photoUrl = story.photoUrl ?: "https://picsum.photos/seed/picsum/200/300",
                             name = story.name ?: "No Name",
-                            description = story.description ?: "No Description"
+                            description = story.description ?: "No Description",
+                            time = ("Created at: " + story.createdAt?.let { formatTimestamp(it) })
                         )
                     }
                     is Result.Error -> {
@@ -87,7 +99,8 @@ fun PreviewDetailStory() {
     DetailStory(
         photoUrl = "https://story-api.dicoding.dev/images/stories/photos-1732633121592_9a49bbb02b35aad5f8a1.jpg",
         name = "Sample Story",
-        description = "This is a description of the story. It is concise and informative."
+        description = "This is a description of the story. It is concise and informative.",
+        time = "22 Jan"
     )
 }
 
@@ -96,6 +109,7 @@ fun DetailStory(
     name: String,
     description: String,
     photoUrl: String,
+    time: String,
     modifier : Modifier = Modifier
 ) {
     Column {
@@ -108,12 +122,31 @@ fun DetailStory(
                 .height(200.dp),
             contentScale = ContentScale.Crop
         )
+        Spacer(modifier = Modifier.height(16.dp))
         Text(
             text = name,
             style = MaterialTheme.typography.titleLarge,
             fontWeight = FontWeight.Bold
         )
-        Spacer(modifier = Modifier.height(4.dp))
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(
+            modifier = modifier,
+            horizontalArrangement = Arrangement.spacedBy(3.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_calendar),
+                contentDescription = "Calendar Icon",
+                tint = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.size(16.dp)
+            )
+            Text(
+                text = time,
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
         Text(
             text = description,
             style = MaterialTheme.typography.bodyMedium,
